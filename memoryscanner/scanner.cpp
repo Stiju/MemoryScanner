2d124fac8001a2df5bc97b75d6c3aca2d9a8797a
Stiju <git@stiju.com>
2016-09-28 15:22:03 +0200

    changed find_next to use valuetypes and comparetypes

--- memoryscanner/scanner.cpp
+++ memoryscanner/scanner.cpp
@@ -57,17 +57,18 @@ auto get_compare_method(CompareType compare_type) {
 	}
 }
 
+union DataValue {
+	int8_t int8;
+	int16_t int16;
+	int32_t int32;
+	int64_t int64;
+	float float_value;
+	double double_value;
+};
+
 void Scanner::find_first(const std::string& value) {
 	std::function<void(uint8_t*, uint8_t*, uint8_t*, int, void*, MemoryResults&)> compare_method;
-	std::cout << sizeof(compare_method) << '\n';
-	union DataValue {
-		int8_t int8;
-		int16_t int16;
-		int32_t int32;
-		int64_t int64;
-		float float_value;
-		double double_value;
-	} dv;
+	DataValue dv;
 	void* val = &dv;
 	switch(settings.value_type) {
 	case ValueType::Int8:
@@ -118,7 +119,88 @@ void Scanner::find_first(const std::string& value) {
 	}
 }
 
+template<typename T, typename Compare = std::equal_to<>>
+MemoryResults::iterator find_next_in_buffer(uint8_t* begin, uint8_t* buffer, size_t buffer_size, MemoryResults::iterator result_it, MemoryResults::iterator result_end, void* value) {
+	T val = *reinterpret_cast<T*>(value);
+	for(; result_it != result_end; ++result_it) {
+		size_t position = result_it->address - begin;
+		if(position >= buffer_size) {
+			break;
+		}
+		T newval = *reinterpret_cast<T*>(buffer + position);
+
+		if(Compare()(newval, val)) {
+			result_it->value = static_cast<int>(newval);
+		} else {
+			result_it->address = nullptr;
+		}
+	}
+	return result_it;
+}
+
+template<>
+MemoryResults::iterator find_next_in_buffer<std::string>(uint8_t* begin, uint8_t* buffer, size_t buffer_size, MemoryResults::iterator result_it, MemoryResults::iterator result_end, void* value) {
+	std::string& val = *reinterpret_cast<std::string*>(value);
+	auto data = val.data();
+	size_t size = val.size();
+	for(; result_it != result_end; ++result_it) {
+		size_t position = result_it->address - begin;
+		if(position >= buffer_size) {
+			break;
+		}
+		if(std::memcmp(buffer + position, data, size) != 0) {
+			result_it->address = nullptr;
+		}
+	}
+	return result_it;
+}
+
+template<typename T>
+auto get_next_compare_method(CompareType compare_type) {
+	switch(compare_type) {
+	case CompareType::Equal: return find_next_in_buffer<T, std::equal_to<>>;
+	case CompareType::Less: return find_next_in_buffer<T, std::less<>>;
+	case CompareType::Greater: return find_next_in_buffer<T, std::greater<>>;
+	case CompareType::Unknown:
+	default: return find_next_in_buffer<int, UnknownValue>;
+	}
+}
+
 void Scanner::find_next(const std::string& value) {
+	std::function<MemoryResults::iterator(uint8_t*, uint8_t*, size_t, MemoryResults::iterator, MemoryResults::iterator, void*)> compare_method;
+	DataValue dv;
+	void* val = &dv;
+	switch(settings.value_type) {
+	case ValueType::Int8:
+		compare_method = get_next_compare_method<int8_t>(settings.compare_type);
+		dv.int8 = static_cast<int8_t>(std::stoi(value));
+		break;
+	case ValueType::Int16:
+		compare_method = get_next_compare_method<int16_t>(settings.compare_type);
+		dv.int16 = static_cast<int16_t>(std::stoi(value));
+		break;
+	case ValueType::Int32:
+		compare_method = get_next_compare_method<int32_t>(settings.compare_type);
+		dv.int32 = static_cast<int32_t>(std::stoi(value));
+		break;
+	case ValueType::Int64:
+		compare_method = get_next_compare_method<int64_t>(settings.compare_type);
+		dv.int64 = static_cast<int64_t>(std::stoll(value));
+		break;
+	case ValueType::Float:
+		compare_method = get_next_compare_method<float>(settings.compare_type);
+		dv.float_value = std::stof(value);
+		break;
+	case ValueType::Double:
+		compare_method = get_next_compare_method<double>(settings.compare_type);
+		dv.double_value = std::stod(value);
+		break;
+	case ValueType::String:
+		compare_method = find_next_in_buffer<std::string, std::equal_to<>>;
+		val = &const_cast<std::string&>(value);
+		break;
+	}
+
 	auto result_it = results.begin();
 	auto result_last = results.end();
 	for(const auto& region : sys_memory_regions()) {
@@ -142,17 +224,12 @@ void Scanner::find_next(const std::string& value) {
 			if(!sys_read_memory(begin, &buffer, bytesToRead, &read)) {
 				std::cout << "Failed to read value at " << static_cast<void*>(begin) << ", read " << read << '/' << bytesToRead << ", error " << sys_get_error() << '\n';
 			}
-			for(; result_it != result_end; ++result_it) {
-				auto position = result_it->address - begin;
-				if(position >= kBufferSize) {
-					break;
-				}
-				result_it->value = *reinterpret_cast<int*>(buffer + position);
-			}
+			result_it = compare_method(begin, buffer, kBufferSize, result_it, result_end, val);
+
 			begin += kBufferSize;
 		}
 	}
-	//results.erase(std::remove_if(results.begin(), results.end(), [value](auto& x) {return x.value != value; }), results.end());
+	results.erase(std::remove_if(results.begin(), results.end(), [](auto& x) {return x.address == nullptr; }), results.end());
 }
 
 void Scanner::find(const std::string& value) {
